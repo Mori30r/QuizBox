9b2be978cedb7b8f7e054db7beae772ae82d2189
amireho3ein <amireedoran@gmail.com>
2023-09-11 13:05:32 +0330

    Update admin configuration for models

--- django/account/admin.py
+++ django/account/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Student, Teacher
 
-admin.site.register(Student)
-admin.site.register(Teacher)
+class TeacherَAdmin(admin.ModelAdmin):
+    list_display = ['email', 'teacher_code']
+    
+
+class StudentَAdmin(admin.ModelAdmin):
+    list_display = ['email', 'student_code']
+
+admin.site.register(Student, StudentَAdmin)
+admin.site.register(Teacher, TeacherَAdmin)
--- django/authentication/admin.py
+++ django/authentication/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import  UserBaseQuizBox
 
-admin.site.register(UserBaseQuizBox)
+class UserBaseQuizBoxAdmin(admin.ModelAdmin):
+    list_display = ['username', 'role', 'phone_number']
+
+admin.site.register(UserBaseQuizBox, UserBaseQuizBoxAdmin)
