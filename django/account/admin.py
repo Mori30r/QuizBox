22f5121ebd06ff7ddb2309037b656d5f687a49f3
amireho3ein <amireedoran@gmail.com>
2023-09-11 20:57:20 +0330

    Revert "Update admin configuration for models"
    This reverts commit 9b2be978cedb7b8f7e054db7beae772ae82d2189.

--- django/account/admin.py
+++ django/account/admin.py
@@ -1,12 +1,5 @@
 from django.contrib import admin
 from .models import Student, Teacher
 
-class TeacherَAdmin(admin.ModelAdmin):
-    list_display = ['email', 'teacher_code']
-    
-
-class StudentَAdmin(admin.ModelAdmin):
-    list_display = ['email', 'student_code']
-
-admin.site.register(Student, StudentَAdmin)
-admin.site.register(Teacher, TeacherَAdmin)
+admin.site.register(Student)
+admin.site.register(Teacher)
--- django/authentication/admin.py
+++ django/authentication/admin.py
@@ -1,7 +1,4 @@
 from django.contrib import admin
 from .models import  UserBaseQuizBox
 
-class UserBaseQuizBoxAdmin(admin.ModelAdmin):
-    list_display = ['username', 'role', 'phone_number']
-
-admin.site.register(UserBaseQuizBox, UserBaseQuizBoxAdmin)
+admin.site.register(UserBaseQuizBox)
