e7c57202e3bf9c89e9a378cf180af060c724ab24
amireho3ein <amireedoran@gmail.com>
2023-09-20 07:19:08 +0330

    add model for app quiz

--- django/quiz/models.py
+++ django/quiz/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+
+class Quiz(models.Model):
+    name = models.CharField(max_length=50)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    duration = models.DurationField()
+    eligible_students = models.ManyToManyField('account.Student', blank=True, related_name='quizzes')
+    course = models.ForeignKey('account.Course', on_delete=models.CASCADE, related_name='quizzes')
+
+
+class Question(models.Model):
+    QUESTION_TYPES = (
+        ('explanatory', 'Explanatory'),
+        ('test', 'Test'),
+    )
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    description = models.CharField(max_length=255)
+    question_type = models.CharField(max_length=12, choices=QUESTION_TYPES)
+    time_limit = models.DurationField()
+
+class QuestionOption(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    text = models.CharField(max_length=255)
+    is_correct = models.BooleanField(default=False)
+
+
+class Submission(models.Model):
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    student = models.ForeignKey('account.Student', on_delete=models.CASCADE)
+    submitted_at = models.DateTimeField(auto_now_add=True)
+    answers = models.JSONField()
+    file_upload = models.FileField(upload_to='submissions/', null=True, blank=True)
+    
--- django/quizbox/urls.py
+++ django/quizbox/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/auth/", include("authentication.urls")),
     path("api/account/", include("account.urls")),
-    path("api/question/", include("quiz.urls")),
+    path("api/quiz/", include("quiz.urls")),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/docs', SpectacularSwaggerView.as_view(url_name="schema"))
 ]
